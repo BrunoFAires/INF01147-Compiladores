395112c11dca1c64c701ab151b25d7a986bbbeea
Bruno Aires <brunoferreiraaires@gmail.com>
2024-10-15 18:18:59 -0300

    fix makefile

--- etapa2/src/util/util.c
+++ etapa2/src/util/util.c
@@ -1,9 +1,18 @@
+#include <stdio.h>
+
 int line_number = 1;
 
-int get_line_number(void){
+int get_line_number(void)
+{
     return line_number;
 }
 
-void increment_line_number(){
+void increment_line_number()
+{
     line_number++;
+}
+
+void yyerror(char const *mensagem)
+{
+    fprintf(stderr, "%s\n", mensagem);
 }
