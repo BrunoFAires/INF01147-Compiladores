634e100f83ca1d2d022bb6e8c218b6a68824a18c
hugobbi <hgugobbi@gmail.com>
2024-12-29 19:11:44 -0300

    cap max number of register

--- etapa6/include/registradores.h
+++ etapa6/include/registradores.h
@@ -4,9 +4,8 @@
 
 #include <stdlib.h>
 
-const char *REGISTRADORES[] = {"rsi", "rdi", "r8", "r9", "r10", "r11"}; // "ebx" "eax", "ecx", "edx"
-static inline int num_regs();
-static inline int num_reg(char *reg) { return atoi(reg + 1); }
-const static inline char *mapeia_registradores(char *reg) { return REGISTRADORES[num_reg(reg)]; }
+int total_num_regs();
+static inline int get_num_reg(char *reg) { return atoi(reg + 1); }
+const char *mapeia_registradores(char *reg);
 
 #endif // _REGISTRADORES_H
--- etapa6/src/registradores.c
+++ etapa6/src/registradores.c
@@ -1,2 +1,13 @@
 #include "registradores.h"
 #include "tabela.h"
+
+const char *REGISTRADORES[] = {"rsi", "rdi", "r8", "r9", "r10", "r11"}; // "ebx" "eax", "ecx", "edx"
+
+const char *mapeia_registradores(char *reg)
+{ 
+    int num_reg = get_num_reg(reg); 
+    num_reg = num_reg < total_num_regs() ? num_reg : 0;
+    return REGISTRADORES[num_reg]; 
+}
+
+int total_num_regs() { return sizeof(REGISTRADORES) / sizeof(REGISTRADORES[0]); }
